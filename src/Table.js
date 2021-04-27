917cb6e06ae0b3cde0fe67e706118161341d2b40
sarah <slachehab6@gmail.com>
2021-04-27 11:58:54 +0200

    Renommez Game.jsx à StartGame.jsx et tous les imports. La classe doit aussi changer de nom. Dans Table.js assurez vous d'être en train de l'appeler aussi avec le nouveau nom.

--- src/Table.js
+++ src/Table.js
@@ -3,7 +3,7 @@ import "./App.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from './componant/outils/Button.jsx';
 import Cartes from "./componant/outils/Cartes";
-import Game from './componant/views/Game.jsx'
+import Game from './componant/views/StartGame.jsx'
 
 const cardArray = [
   "KS", "QS", "JS", "AS", "2S", "3S", "4S", "5S", "6S", "7S", "8S", "9S", "0S",
